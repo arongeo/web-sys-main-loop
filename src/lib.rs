14728fa76c336d57fed45bbe9a6107e8c038089f
arongeo <me@arongeo.com>
2024-01-14 13:01:42 +0100

    Don't need prelude for small package

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,7 @@ mod frame_state;
 mod input_handler;
 mod main_loop;
 
-pub mod prelude {
-    pub use crate::frame_state::FrameState;
-    pub use crate::input_handler::mouse_state::MouseButton;
-    pub use crate::input_handler::mouse_state::MouseState;
-    pub use crate::main_loop::start;
-}
+pub use crate::frame_state::FrameState;
+pub use crate::input_handler::mouse_state::MouseButton;
+pub use crate::input_handler::mouse_state::MouseState;
+pub use crate::main_loop::start;
